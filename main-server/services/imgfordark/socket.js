2b032915f4073084966f1cb50f8c6fd3e97fa93f
Rerepopo <dojun2006@gmail.com>
2023-01-03 21:15:42 +0900

    Use dotenv on the main server

--- main-server/services/imgfordark/socket.js
+++ main-server/services/imgfordark/socket.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
+
 const { io } = require("socket.io-client");
-const socket = io("http://127.0.0.1:5505").connect();
+const socket = io("http://" +  process.env.SERVER_HOST + ":" + process.env.SERVER_PORT).connect();
 
 socket.on("connect", () => {
   console.log("Socket connected: " + socket.id);
